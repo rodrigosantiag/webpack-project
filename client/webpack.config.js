0f29d2f0461c94c9f87ec2cca81a03ce5a985a50
Rodrigo Santiago <rodrigosantiag@gmail.com>
2021-03-20 13:39:14 -0300

    Lazy loading for negociacao_service Encapsulating and minifying index.html

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,16 +3,33 @@ const babiliPlugin = require('babili-webpack-plugin');
 const extractTextPlugin = require('extract-text-webpack-plugin')
 const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const webpack = require('webpack')
+const htmlWebPackPlugin = require('html-webpack-plugin');
 
 let plugins = [];
 
+plugins.push(new htmlWebPackPlugin({
+    hash: true,
+    minify: {
+        html5: true,
+        collapseWhitespace: true,
+        removeComments: true
+    },
+    filename: 'index.html',
+    template: __dirname + '/main.html'
+}));
 plugins.push(new extractTextPlugin('styles.css'))
 plugins.push(new webpack.ProvidePlugin({
     '$': 'jquery/dist/jquery.js',
     'jQuery': 'jquery/dist/jquery.js'
 }));
 
+plugins.push(new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    filename: 'vendor.bundle.js'
+}));
+
 if(process.env.NODE_ENV == 'production'){
+    plugins.push(new webpack.optimize.ModuleConcatenationPlugin())
     plugins.push(new babiliPlugin());
     plugins.push(new optimizeCSSAssetsPlugin({
         cssProcessor: require('cssnano'),
@@ -26,11 +43,13 @@ if(process.env.NODE_ENV == 'production'){
 }
 
 module.exports = {
-    entry: './app-src/app.js',
+    entry: {
+        app: './app-src/app.js',
+        vendor: ['jquery', 'bootstrap', 'reflect-metadata']
+    },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: 'dist'
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
